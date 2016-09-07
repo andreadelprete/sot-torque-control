fc26a93bec3d256506ce35823f949727f906454c
Thomas Flayols <tflayols@laas.fr>
2016-09-07 16:14:15 +0200

    add template class for 7 arguments commands

--- include/sot/torque_control/commands-helper.hh
+++ include/sot/torque_control/commands-helper.hh
@@ -222,6 +222,100 @@ namespace dynamicgraph {
               +"Void return.\n\n" );
     }
 
+    template <class E,typename T1,typename T2,typename T3,typename T4,typename T5,typename T6,typename T7 >
+    struct CommandVoid7
+        : public Command
+    {
+      typedef boost::function<void(const T1&,const T2&,const T3&,const T4&,const T5&,const T6&,const T7&)> function_t;
+      typedef boost::function<void(E*,const T1&,const T2&,const T3&,const T4&,const T5&,const T6&,const T7&)> memberFunction_t;
+      typedef void (E::*memberFunction_ptr_t) (const T1&,const T2&,const T3&,const T4&,const T5&,const T6&,const T7&);
+
+      CommandVoid7(E& entity, function_t function,
+                   const std::string& docString)
+        :Command(entity,
+                 boost::assign::list_of
+                 (ValueHelper<T1>::TypeID)
+                 (ValueHelper<T2>::TypeID)
+                 (ValueHelper<T3>::TypeID)
+                 (ValueHelper<T4>::TypeID)
+                 (ValueHelper<T5>::TypeID)
+                 (ValueHelper<T6>::TypeID)
+                 (ValueHelper<T7>::TypeID)
+                 , docString)
+        ,fptr(function)
+      {}
+
+    protected:
+      virtual Value doExecute()
+      {
+        assert( getParameterValues().size() == 7 );
+        T1 val1 = getParameterValues()[0].value();
+        T2 val2 = getParameterValues()[1].value();
+        T3 val3 = getParameterValues()[2].value();
+        T4 val4 = getParameterValues()[3].value();
+        T5 val5 = getParameterValues()[4].value();
+        T6 val6 = getParameterValues()[5].value();
+        T7 val7 = getParameterValues()[6].value();
+        fptr(val1,val2,val3,val4,val5,val6,val7);
+        return Value(); // void
+      }
+    private:
+      function_t fptr;
+    };
+
+
+    template <class E,typename T1,typename T2,typename T3,typename T4,typename T5,typename T6,typename T7 >
+    CommandVoid7<E,T1,T2,T3,T4,T5,T6,T7>*
+    makeCommandVoid7(E& entity,
+                     typename CommandVoid7<E,T1,T2,T3,T4,T5,T6,T7>::function_t function ,
+                     const std::string& docString)
+    {
+      return new CommandVoid7<E,T1,T2,T3,T4,T5,T6,T7>( entity,function,docString );
+    }
+
+    template <class E,typename T1,typename T2,typename T3,typename T4,typename T5,typename T6,typename T7 >
+    CommandVoid7<E,T1,T2,T3,T4,T5,T6,T7>*
+    makeCommandVoid7(E& entity,
+                     boost::function<void(E*,const T1&,const T2&,const T3&,const T4&,const T5&,const T6&,const T7&)> function,
+                     const std::string& docString)
+    {
+      return new CommandVoid7<E,T1,T2,T3,T4,T5,T6,T7>( entity,
+                                                    boost::bind(function,&entity,_1,_2,_3,_4,_5,_6,_7),docString );
+    }
+
+    template <class E,typename T1,typename T2,typename T3,typename T4,typename T5,typename T6,typename T7 >
+    CommandVoid7<E,T1,T2,T3,T4,T5,T6,T7>*
+    makeCommandVoid7(E& entity,
+                     void (E::*function) (const T1&,const T2&,const T3&,const T4&,const T5&,const T6&,const T7&),
+                     const std::string& docString)
+    {
+      return new CommandVoid7<E,T1,T2,T3,T4,T5,T6,T7>( entity,
+                                                          boost::bind(function,&entity,_1,_2,_3,_4,_5,_6,_7),
+                                                          docString );
+      return NULL;
+    }
+
+    inline std::string docCommandVoid7( const std::string& doc,
+                                        const std::string& type1,
+                                        const std::string& type2,
+                                        const std::string& type3,
+                                        const std::string& type4,
+                                        const std::string& type5,
+                                        const std::string& type6,
+                                        const std::string& type7)
+    {
+      return (std::string("\n")+doc+"\n\n"
+              +"Input:\n - A "+type1+".\n"
+              +"Input:\n - A "+type2+".\n"
+              +"Input:\n - A "+type3+".\n"
+              +"Input:\n - A "+type4+".\n"
+              +"Input:\n - A "+type5+".\n"
+              +"Input:\n - A "+type6+".\n"
+              +"Input:\n - A "+type7+".\n"
+              +"Void return.\n\n" );
+    }
+
+
   } // namespace command
 } // namespace dynamicgraph
 
