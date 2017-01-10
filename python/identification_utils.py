9573a3e8636c989b348b9ae6663734229f9de86a
Thomas Flayols <tflayols@laas.fr>
2017-01-10 11:47:56 +0100

    add identification trajectories for LLEG

--- python/identification_utils.py
+++ python/identification_utils.py
@@ -66,7 +66,6 @@ def identify_rhy_static(traj_gen,staticTime=60.0):
   time.sleep(staticTime)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 def identify_rhy_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   (joint, min_pos, max_pos) = ('rhy', -0.7, 0.45)
   go_to_zero_position(traj_gen,5.0)
@@ -76,11 +75,9 @@ def identify_rhy_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
-
 #(-0.610865, 0.349066);  #// right hip roll ****************************
-(joint, min_pos, max_pos) = ('rhr', -0.5, 0.25)
 def identify_rhr_static(traj_gen,staticTime=60.0):
+  (joint, min_pos, max_pos) = ('rhr', -0.5, 0.25)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
   traj_gen.moveJoint('lhr',0.25,5.0)
@@ -88,7 +85,6 @@ def identify_rhr_static(traj_gen,staticTime=60.0):
   time.sleep(staticTime)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 def identify_rhr_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0,2.5]):
   (joint, min_pos, max_pos) = ('rhr', -0.5, 0.25)
   go_to_zero_position(traj_gen,5.0)
@@ -102,7 +98,6 @@ def identify_rhr_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0,2.5]):
   doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 #(-2.18166, 0.733038);   #// right hip pitch ***************************:
 def identify_rhp_static(traj_gen,staticTime=60.0):
   go_to_zero_position(traj_gen,5.0)
@@ -111,7 +106,6 @@ def identify_rhp_static(traj_gen,staticTime=60.0):
   time.sleep(staticTime)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 def identify_rhp_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   (joint, min_pos, max_pos) = ('rhp', -1.7, 0.6)
   go_to_zero_position(traj_gen,5.0)
@@ -121,7 +115,6 @@ def identify_rhp_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 #(-0.0349066, 2.61799);  #// right knee ********************************
 def identify_rk_static(traj_gen,staticTime=60.0):
   go_to_zero_position(traj_gen,5.0)
@@ -132,7 +125,6 @@ def identify_rk_static(traj_gen,staticTime=60.0):
   time.sleep(staticTime)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 def identify_rk_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   (joint, min_pos, max_pos) = ('rk', 0., 2.5)
   go_to_zero_position(traj_gen,5.0)
@@ -152,7 +144,6 @@ def identify_rap_static(traj_gen,staticTime=60.0):
   time.sleep(staticTime)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 def identify_rap_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   (joint, min_pos, max_pos) = ('rap', -1.2, 0.6)
   go_to_zero_position(traj_gen,5.0)
@@ -163,7 +154,6 @@ def identify_rap_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 #(-0.349066, 0.610865);  #// right ankle roll **************************
 def identify_rar_static(traj_gen,staticTime=60.0):
   go_to_zero_position(traj_gen,5.0)
@@ -174,7 +164,6 @@ def identify_rar_static(traj_gen,staticTime=60.0):
   time.sleep(staticTime)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
-
 def identify_rar_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   (joint, min_pos, max_pos) = ('rar', -0.25, 0.5)
   go_to_zero_position(traj_gen,5.0)
@@ -185,6 +174,126 @@ def identify_rar_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
   doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
   go_to_zero_position(traj_gen,5.0)
   time.sleep(5.0 + 0.5)
+
+
+#(-0.785398, 0.523599);  #// left hip yaw *********************INVERTED
+def identify_lhy_static(traj_gen,staticTime=60.0):
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lhp',-1.57,5.0)
+  time.sleep(5.0)
+  time.sleep(staticTime)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+def identify_lhy_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
+  (joint, min_pos, max_pos) = ('lhy', +0.7, -0.45)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('rhr',-0.1,3.0)
+  time.sleep(3.0 + 0.5)
+  doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+#(-0.610865, 0.349066);  #// left hip roll ********************INVERTED
+def identify_lhr_static(traj_gen,staticTime=60.0):
+  (joint, min_pos, max_pos) = ('lhr', +0.5, -0.25)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('rhr',-0.25,5.0)
+  time.sleep(5.0)
+  time.sleep(staticTime)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+def identify_lhr_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0,2.5]):
+  (joint, min_pos, max_pos) = ('lhr', +0.5, -0.25)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lsp',-1.57,5.0)
+  traj_gen.moveJoint('rsp',-1.57,5.0)
+  traj_gen.moveJoint('le',-1.57,5.0)
+  traj_gen.moveJoint('re',-1.57,5.0)
+  traj_gen.moveJoint('rhr',-0.25,5.0)
+  time.sleep(5.0 + 0.5)
+  doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+#(-2.18166, 0.733038);   #// left hip pitch ***************************:
+def identify_lhp_static(traj_gen,staticTime=60.0):
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  time.sleep(5.0)
+  time.sleep(staticTime)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+def identify_lhp_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
+  (joint, min_pos, max_pos) = ('lhp', -1.7, 0.6)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lhr',+0.2,5.0)
+  time.sleep(5.0 + 0.5)
+  doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+#(-0.0349066, 2.61799);  #// left knee ********************************
+def identify_lk_static(traj_gen,staticTime=60.0):
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lhp',-1.57,5.0)
+  traj_gen.moveJoint('lk',1.57,5.0)
+  time.sleep(5.0 + 0.5)
+  time.sleep(staticTime)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+def identify_lk_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
+  (joint, min_pos, max_pos) = ('lk', 0., 2.5)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lhp',-1.57,5.0)
+  time.sleep(5.0 + 0.5)
+  doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+#(-1.309, 0.733038);     #// left ankle pitch *************************
+def identify_lap_static(traj_gen,staticTime=60.0):
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lhp',-1.57,5.0)
+  traj_gen.moveJoint('lk',1.57,5.0)
+  time.sleep(5.0 + 0.5)
+  time.sleep(staticTime)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+def identify_lap_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
+  (joint, min_pos, max_pos) = ('lap', -1.2, 0.6)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lhp',-1.57,5.0)
+  traj_gen.moveJoint('lk',1.57,5.0)
+  time.sleep(5.0 + 0.5)
+  doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+#(-0.349066, 0.610865);  #// left ankle roll ******************INVERTED
+def identify_lar_static(traj_gen,staticTime=60.0):
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lhp',-1.57,5.0)
+  traj_gen.moveJoint('lk',1.57,5.0)
+  time.sleep(5.0 + 0.5)
+  time.sleep(staticTime)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+def identify_lar_dynamic(traj_gen,mode='constAcc',N=3,times=[5.0,4.0,3.0]):
+  (joint, min_pos, max_pos) = ('lar', +0.25, -0.5)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+  traj_gen.moveJoint('lhp',-1.57,5.0)
+  traj_gen.moveJoint('lk',1.57,5.0)
+  time.sleep(5.0 + 0.5)
+  doNCycles(traj_gen,joint,min_pos, max_pos,N,times,mode)
+  go_to_zero_position(traj_gen,5.0)
+  time.sleep(5.0 + 0.5)
+
 
 def go_to_zero_position(traj_gen,T=10.0):
 # Python interpreter can't deal with input(..) ??
